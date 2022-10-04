6f19a760c7336a3edd84f412e3cce9c2cecd9d97
Hemant <87967889+Hemant2801@users.noreply.github.com>
2022-10-04 17:32:29 +0530

    Create Video_player.py

---
+++ Video_player.py
@@ -0,0 +1,9 @@
+import cv2
+    
+cap = cv2.VideoCapture(0)
+while True:
+    _, frame = cap.read()
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1)
+    if key == 27: # Esc key
+        break
